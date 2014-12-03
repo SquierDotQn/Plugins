b4c183572007088eee76a4661c769cc4afa4e61f
SquierDotQn <gamefreak35@hotmail.fr>
2014-12-03 17:16:42 +0100

    Presque fini pour la base graphique

--- src/window/WritingPanel.java
+++ src/window/WritingPanel.java
@@ -1,6 +1,8 @@
 package window;
 
 import java.awt.FlowLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -8,6 +10,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 
 import model.WritingModel;
@@ -25,22 +29,22 @@ public class WritingPanel extends JPanel implements Observer{
 	protected final JTextArea textfield;
 	protected final JScrollPane sp;
 
-	public WritingPanel(WritingModel wm) {
+	public WritingPanel( WritingModel wm) {
 		super();
 		this.wm = wm;
 		setLayout(new FlowLayout());
 		textfield = new JTextArea(5, 35);
 		textfield.setText(wm.getText());
 		sp = new JScrollPane(textfield);
+		// textfield.; METTRE UN OBSERVER QUI VERIFIE QUAND LE TEXTE DU TEXTFIELD CHANGE, ET FAIRE UN wm.setText(textfield.getText());
 		this.add(sp);
 		setVisible(true);
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
-		
+		textfield.setText(wm.getText());
 	}
 
-
+	
 }
