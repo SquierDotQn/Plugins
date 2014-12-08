ccf5eccd9330859dcac317341b499639ea8c5734
Théo Plockyn <gamefreak35@hotmail.fr>
2014-12-08 23:07:07 +0100

    Javadoc

--- src/window/MainWindow.java
+++ src/window/MainWindow.java
@@ -8,13 +8,21 @@ import javax.swing.JMenuBar;
 
 import menus.TopMenuBar;
 import model.WritingModel;
-
+/**
+ * This class is the main window of the text editor, it contains the model ( WritingModel ),
+ * the menubar  and the text area ( WritingPanel ).
+ * @author Théo Plockyn & Valentin Caulier
+ */
 @SuppressWarnings("serial")
 public class MainWindow extends JFrame{
 	private final WritingPanel wp;
 	private final WritingModel wm;
 	private final JMenuBar mb;
 	
+	/**
+	 * The constructor initialize the content of the window, build it then set the window visible.
+	 * @args WritingModel wm the model
+	 */
 	public MainWindow(WritingModel wm){
 		super("Plugin");
 		this.wm = wm;
@@ -25,6 +33,9 @@ public class MainWindow extends JFrame{
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
+	/**
+	 * Builds all the content of the window, sets the content to their places
+	 */
 	public void buildContentPane(){
 		Container c = this.getContentPane();
 		c.setLayout(new BorderLayout());
