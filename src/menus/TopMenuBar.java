01a5d240ee0cd84e35c6117e01a160662ca2ce75
Théo Plockyn <gamefreak35@hotmail.fr>
2014-12-08 23:23:42 +0100

    Javadoc

--- src/menus/TopMenuBar.java
+++ src/menus/TopMenuBar.java
@@ -10,6 +10,10 @@ import javax.swing.JMenuItem;
 
 import model.WritingModel;
 
+/**
+ * This class is the top bar containing all three menus
+ * @author Théo Plockyn & Valentin Caulier
+ */
 @SuppressWarnings("serial")
 public class TopMenuBar extends JMenuBar{
 	
@@ -26,15 +30,15 @@ public class TopMenuBar extends JMenuBar{
 	}
 
 	/**
-	 * Crée les onglets Fichier, Edition, Affichage et "?".
+	 * Creates the menus File, Tools and Help
 	 */
 	public void build(){
 		MenuListener ml = new MenuListener();
-		file = new JMenuItem("Fichier");
+		file = new JMenuItem("File");
 		file.addActionListener(ml);
-		tools = new JMenuItem("Edition");
+		tools = new JMenuItem("Tools");
 		tools.addActionListener(ml);
-		help = new JMenuItem("?");
+		help = new JMenuItem("Help");
 		help.addActionListener(ml);
 		setLayout(new BorderLayout());
 		this.add(file, BorderLayout.WEST);
@@ -44,7 +48,8 @@ public class TopMenuBar extends JMenuBar{
 	}
 	
 	/**
-	 * Listener de la barre de menu.
+	 * MenuBar listener, when clicked show the appropriate menu
+	 * @author Théo Plockyn & Valentin Caulier
 	 */
 	class MenuListener implements ActionListener{
 
