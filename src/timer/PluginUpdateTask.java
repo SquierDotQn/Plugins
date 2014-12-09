3d630914cc13c1449f0967615da35cebdb891f65
Valentin CAULIER <vcaulier@ymail.com>
2014-12-09 09:45:27 +0100

    PluginUpdateTask JavaDoc

--- src/timer/PluginUpdateTask.java
+++ src/timer/PluginUpdateTask.java
@@ -5,21 +5,33 @@ import java.util.TimerTask;
 
 import model.PluginFinder;
 
+/**
+ * A TimerTask adapted to a Plugin's update
+ */
 public class PluginUpdateTask extends TimerTask {
 
+	/**
+	 * A PluginFinder, will be used by the TimerTask
+	 */
 	private PluginFinder finder;
 	
+	/**
+	 * Constructor of a PluginUpdateTask for a given PluginFinder
+	 * @param finder The PluginFinder the TimerTask will wake up and update
+	 */
 	public PluginUpdateTask(PluginFinder finder){
 		super();
 		this.finder = finder;
 	}
 
 	@Override
+	/**
+	 * The method run() of the TimerTask will ask the PluginFinder to update it's current Plugin's File list
+	 */
 	public void run() {
 		try {
 			this.finder.updateFiles();
 		} catch (ClassNotFoundException | IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
